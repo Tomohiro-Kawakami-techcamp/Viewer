a117f61397470d86e6c8eb1c26940a6c2f31762e
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-02-14 21:24:29 +0900

    完了

--- app/src/main/java/com/example/viewer/ViewerItemActivity.java
+++ app/src/main/java/com/example/viewer/ViewerItemActivity.java
@@ -1,18 +1,38 @@
 package com.example.viewer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewerItemActivity extends AppCompatActivity implements View.OnTouchListener {
 
+    private String mPict;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item);
+
+        Intent intent = getIntent();
+        Bundle ex = intent.getExtras();
+        if(ex != null){
+            mPict = ex.getString("PICT");
+        }
+
+        ImageView imageView = (ImageView)findViewById(R.id.imageView1);
+        if("マリノスケ".equals(mPict)){
+            imageView.setImageResource(R.drawable.IMG_0009);
+        }else if("チャンピオンフラッグ1".equals(mPict)){
+            imageView.setImageResource(R.drawable.IMG_0031);
+        }else if("チャンピオンフラッグ2".equals(mPict)){
+            imageView.setImageResource(R.drawable.IMG_1445);
+        }
+
         Button backBtn = (Button)findViewById(R.id.backbutton);
         backBtn.setOnTouchListener(this);
         Button emailBtn = (Button)findViewById(R.id.emailbutton);
