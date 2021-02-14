9da0a8ff8f671511951a6c7a25207923d90b0342
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-02-14 17:44:22 +0900

    main編集

--- app/src/main/java/com/example/viewer/MainActivity.java
+++ app/src/main/java/com/example/viewer/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.viewer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = (ListView)findViewById(R.id.listView1);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
     }
 }
